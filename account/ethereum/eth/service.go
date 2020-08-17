dc90818cb90e408a7954f6d2016e7adb08386e02
AgwaB <leesd556@gmail.com>
2020-08-17 15:13:43 +0900

    feat: add ethereum account skel

--- account/ethereum/eth/service.go
+++ account/ethereum/eth/service.go
@@ -11,10 +11,6 @@ func NewService() *Service {
 	return nil
 }
 
-func (s *Service) DeriveAccount(key keychain.Key) (types.Account, error) {
-	return types.Account{}, nil
-}
-
 func (s *Service) Transfer(key keychain.Key, to string, amount uint) (types.Transaction, error) {
 	return types.Transaction{}, nil
 }
